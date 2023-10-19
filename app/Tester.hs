978c697c31c84f71824a504847baad36e749c415
amhsm6 <amhsm6@gmail.com>
2023-10-19 22:36:16 +0300

    commit

--- app/Tester.hs
+++ app/Tester.hs
@@ -1,44 +1,18 @@
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE MultiParamTypeClasses #-} 
-{-# LANGUAGE FlexibleInstances #-} 
-{-# LANGUAGE UndecidableInstances #-}
 
 module Tester where
 
 import Prelude hiding (log)
 import Control.Monad
 import Control.Monad.Writer
-import Control.Exception
+import Control.Monad.EitherT
+import Control.Exception (try, IOException)
 import Data.Time.Clock.POSIX
-import System.IO
+import System.IO (hGetContents, hPutStrLn, hFlush)
 import System.Process
 import System.Exit hiding (die)
 import System.Directory
 import System.FilePath
-
-newtype EitherT m a b = EitherT (m (Either a b))
-
-runEitherT :: EitherT m a b -> m (Either a b)
-runEitherT (EitherT m) = m
-
-instance Monad m => Monad (EitherT m a) where
-    return = pure
-    (EitherT m) >>= g = EitherT $ m >>= either (pure . Left) (runEitherT . g)
-
-instance Monad m => Applicative (EitherT m a) where
-    pure = EitherT . pure . Right
-    m1 <*> m2 = m1 >>= \f -> m2 >>= pure . f
-
-instance Monad m => Functor (EitherT m a) where
-    fmap f m = m >>= pure . f
-
-instance MonadIO m => MonadIO (EitherT m a) where
-    liftIO io = EitherT $ liftIO io >>= pure . Right
-
-instance MonadWriter w m => MonadWriter w (EitherT m a) where
-    tell w = EitherT $ tell w >>= pure . Right
-    listen (EitherT m) = EitherT $ listen m >>= \(e, w) -> pure $ e >>= Right . (,w)
-    pass (EitherT m) = EitherT $ m >>= either (pure . Left) (pass . pure >=> pure . Right)
 
 data Test = TestGeneric String
           | TestSimple Language [(String, String)]
@@ -46,12 +20,10 @@ data Test = TestGeneric String
 
 data Language = Haskell | C | Cpp | Python
 
-data TestError = TestNotFound
-               | TestGenericFailed
+data TestError = TestGenericFailed
                | TestSimpleCompileError
                | TestSimpleFailed
                | TestCustomFailed
-               deriving Show
 
 type TestLogs = [String]
 
